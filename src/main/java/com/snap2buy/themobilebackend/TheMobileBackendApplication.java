5ceaa155aaa65bcfcacf74c5bd47e44b8a359474
Prasad-Coditas <prasad.lengare@coditas.com>
2020-08-21 13:36:48 +0530

    init commit

---
+++ src/main/java/com/snap2buy/themobilebackend/TheMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snap2buy.themobilebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMobileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snap2buy/themobilebackend/TheMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snap2buy.themobilebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
